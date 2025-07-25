5b27d1952a0391e3e6cfeaab9d1f53c37ea887c0
A-Tagir <191856809+A-Tagir@users.noreply.github.com>
2025-07-25 17:30:50 +0300

    Add balancer

--- main/balancer.tf
+++ main/balancer.tf
@@ -1,1 +1,19 @@
 #http balancer
+# HTTP Router
+resource "yandex_alb_http_router" "router" {
+  name = "k8s-router"
+}
+
+# Target Group для всех worker-узлов
+resource "yandex_lb_target_group" "balancer-group" {
+  name       = "k8s-target-group"
+  depends_on = [yandex_compute_instance.k8s]
+
+  dynamic "target" {
+    for_each = { for k, v in yandex_compute_instance.k8s : k => v if k != "0" }
+    content {
+      subnet_id = target.value.network_interface.0.subnet_id
+      address   = target.value.network_interface.0.ip_address
+    }
+  }
+}
